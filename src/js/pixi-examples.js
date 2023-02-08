527b93e66b8cb0719eaf2ef54f2e8a982416b51a
Anonymous <anonymous@github.com>
2023-02-08 12:36:54 +0000

    .

--- src/js/pixi-examples.js
+++ src/js/pixi-examples.js
@@ -229,7 +229,7 @@ jQuery(document).ready(($) => {
             if (bpc.pixiVersionString === 'local') {
                 pixiUrl = 'dist/pixi.js';
             } else { // other versions come from S3
-                pixiUrl = `https://d157l7jdn8e5sf.cloudfront.net/${bpc.pixiVersionString}/pixi-legacy.js`;
+                pixiUrl = `https://d157l7jdn8e5sf.cloudfront.net/${bpc.pixiVersionString}/pixi.js`;
             }
 
             let html = '<!DOCTYPE html><html><head><style>';
